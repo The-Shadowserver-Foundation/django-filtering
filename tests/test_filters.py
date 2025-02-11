482567a8f91106d4dea863b286c8d71717c59ee8
Michael Mulich <michael.mulich@gmail.com>
2025-02-11 15:21:05 -0800

    Add tests for the existing Filter functionality

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -1,3 +1,4 @@
+import pytest
 import django.db.models
 
 from django_filtering import filters
@@ -103,3 +104,50 @@ class TestChoiceLookup:
             'choices': static_choices,
         }
         assert options_schema_blurb == expected
+
+
+class TestFilter:
+    """
+    Test Filter behavior
+    """
+
+    def test_init_wo_label(self):
+        with pytest.raises(ValueError) as exc_info:
+            filters.Filter(filters.InputLookup('icontains', label='contains'), default_lookup='icontains')
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "At this time, the filter label must be provided."
+
+    def test_init_wo_default_lookup(self):
+        with pytest.raises(ValueError) as exc_info:
+            filters.Filter(filters.InputLookup('icontains', label='contains'), label='name')
+        assert exc_info.type is ValueError
+        assert exc_info.value.args[0] == "At this time, the default_lookup must be provided."
+
+    def test_get_options_schema_info(self):
+        label = "Pages"
+        field = django.db.models.IntegerField(name='pages')
+        lookups_data = (
+            [filters.InputLookup, ('gte',), {'label': '>='}],
+            [filters.InputLookup, ('lte',), {'label': '<='}],
+            [filters.InputLookup, ('exact',), {'label': '='}],
+        )
+        default_lookup = 'exact'
+
+        # Target
+        filter = filters.Filter(
+            *[cls(*a, **kw) for cls, a, kw in lookups_data],
+            default_lookup=default_lookup,
+            label=label,
+        )
+
+        # Check options schema output
+        options_schema_info = filter.get_options_schema_info(field)
+        expected = {
+            'default_lookup': default_lookup,
+            'label': label,
+            'lookups': {
+                a[0]: {'label': kw['label'], 'type': cls.type}
+                for cls, a, kw in lookups_data
+            },
+        }
+        assert options_schema_info == expected
