bac480cc8d08cce86abe16ef663ac70950feb8ef
Michael Mulich <michael.mulich@gmail.com>
2025-02-14 14:39:22 -0800

    Make utility function for constructing the field lookup name

--- src/django_filtering/utils.py
+++ src/django_filtering/utils.py
@@ -1,7 +1,8 @@
-def construct_field_lookup_arg(field, value=None, lookup=None):
+from typing import List, Optional
+
+def construct_field_lookup_name(field_name, lookup: Optional[str | List[str]] = None) -> str:
     """
-    Given a __query data__ structure make a field lookup value
-    that can be used as an argument to ``Q``.
+    Given a field name and lookup, produce a valid argument query filter argument name.
     """
     sequence_types = (
         list,
@@ -11,7 +12,15 @@ def construct_field_lookup_arg(field, value=None, lookup=None):
     if lookup is not None:
         is_lookup_seq = isinstance(lookup, sequence_types)
         lookup_expr = "__".join(['', *lookup] if is_lookup_seq else ['', lookup])
-    return (f"{field}{lookup_expr}", value)
+    return f"{field_name}{lookup_expr}"
+
+
+def construct_field_lookup_arg(field, value=None, lookup=None):
+    """
+    Given a __query data__ structure make a field lookup value
+    that can be used as an argument to ``Q``.
+    """
+    return (construct_field_lookup_name(field, lookup=lookup), value)
 
 
 def merge_dicts(*args):
