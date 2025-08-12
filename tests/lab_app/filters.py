1d66a108c6bea34fd0569b805b985ce7dd83596b
Michael Mulich <michael.mulich@gmail.com>
2025-08-12 13:53:59 -0700

    Enable the use of a custom translator
    Allow the developer to define a custom translator function, which can
    be used to provide more complex filtering criteria.
    
    This feature enables side effects to translate to  filter
    criteria. Some examples uses of this include:
    - psuedo fields
    - made up datapoints
    - api data points
    - static data
    - cross database queries
    
    The translation testcase illustrates a usecase where the model does
    not have a `continent` field, but is able to translate the continent
    into country codes we can use to filter the model. The underlying
    logic for this is hardcoded, but could anything from a api call to a
    cross database query.
    
    Admittedly, the solution provided for this feature is not the best. I
    suspect future iterations will clean up the interface for
    this. Perhaps I'll look at special methods on the FilterSet, similar
    to the django form's field cleaning methods.

--- tests/lab_app/filters.py
+++ tests/lab_app/filters.py
@@ -1,6 +1,7 @@
 import django_filtering as filtering
 
 from . import models
+from . import utils
 
 
 class ParticipantFilterSet(filtering.FilterSet):
@@ -12,3 +13,24 @@ class ParticipantFilterSet(filtering.FilterSet):
 
     class Meta:
         model = models.Participant
+
+
+class StudyFilterSet(filtering.FilterSet):
+    name = filtering.Filter(
+        filtering.InputLookup('icontains', label='contains'),
+        default_lookup='icontains',
+        label="Name",
+    )
+    continent = filtering.Filter(
+        filtering.ChoiceLookup(
+            "exact",
+            label="is",
+            choices=utils.CONTINENT_CHOICES,
+        ),
+        default_lookup='exact',
+        label="Continent",
+        translator=utils.continent_to_countries,
+    )
+
+    class Meta:
+        model = models.Study
